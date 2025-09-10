66cecb31f58679c95374ce77ad568e5230edb85a
sandro8ponali <phongtn@twendeesoft.com>
2025-09-10 09:24:24 +0700

    feat: Add AuthProvider to the root route for authentication context

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,12 +3,14 @@ import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanstackDevtools } from "@tanstack/react-devtools";
 import { Web3Provider } from "@/provider/web3-providers";
 import ChakraUIProvider from "@/provider/chakra-provider";
+import { AuthProvider } from "@/provider/auth-provider";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <ChakraUIProvider>
         <Web3Provider>
+          <AuthProvider>
           <Outlet />
           <TanstackDevtools
             config={{
@@ -21,6 +23,7 @@ export const Route = createRootRoute({
               },
             ]}
           />
+          </AuthProvider>
         </Web3Provider>
       </ChakraUIProvider>
     </>
